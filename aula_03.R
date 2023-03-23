b5f15b0b57d595d8a40b675ceecfaf6bd6d31d0f
ThiagoMoraesRizzieri <rizzierithiago@gmail.com>
2023-03-23 15:35:35 -0300

    Commit com erro

--- aula_03.R
+++ aula_03.R
@@ -7,4 +7,5 @@ y = c(2, 4, 6, 8)
 df = data.frame(x, y)
 
 ggplot()+
-  geom_point(aes(x, y), colour="red", size = 3)
+  geom_point(aes(x, y), colour="red", size = 3) +
+  abline(lsfit(1:10),(1:10))
